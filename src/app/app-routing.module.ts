b42a6b88a01f8e9cc997325ab67093ba3d1b2434
Angel1104 <flores1102angel266@gmail.com>
2023-10-19 17:22:23 -0400

    antes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {
-    path: 'events',
-    component: HomeComponent
-  }
+  // {
+  //   path: 'events',
+  //   component: HomeComponent
+  // }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardCompetenciaComponent } from './components/card-competencia/card-competencia.component';
+import { CardEventoComponent } from './components/card-evento/card-evento.component';
+import { CardNovedadesComponent } from './components/card-novedades/card-novedades.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SidebarComponent,
     NavbarComponent,
-    CardComponent
+    CardComponent,
+    FooterComponent,
+    CardCompetenciaComponent,
+    CardEventoComponent,
+    CardNovedadesComponent
   ],
   imports: [
     BrowserModule,
