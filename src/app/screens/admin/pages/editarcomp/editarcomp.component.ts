31ee0c48be48debb9d16eb1e088dcc24f03a1f3c
VinniMagne <vinni.magne@gmail.com>
2023-11-11 08:28:40 -0400

    visualizar comp - eliminar comp - editar comp

--- src/app/screens/admin/pages/editarcomp/editarcomp.component.ts
+++ src/app/screens/admin/pages/editarcomp/editarcomp.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../../../services/api/api.service';
+import { CompetenciaEditI } from "../../../../models/competencia.interface";
+import { FormGroup, Validators, FormControl } from "@angular/forms";
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { CompetenciaI } from 'src/app/models/competenciaComp.interface';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'YYYY/MM/DD',
+    monthYearLabel: 'YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'YYYY',
+  },
+};
 
 @Component({
   selector: 'app-editarcomp',
   templateUrl: './editarcomp.component.html',
-  styleUrls: ['./editarcomp.component.css']
+  styleUrls: ['./editarcomp.component.css'],
+  providers: [{ provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }],
 })
-export class EditarcompComponent {
+export class EditarcompComponent implements OnInit {
+  constructor(private router: Router, private activaterouter:ActivatedRoute,private apiService: ApiService) {}
 
+  dataCompetencia! : CompetenciaI;
+  editarForm = new FormGroup({
+    nombre: new FormControl('',Validators.required),
+    descripcion : new FormControl('',Validators.required),
+    encargado : new FormControl('',Validators.required),
+    fechaFin : new FormControl('',Validators.required),
+    fechaIni : new FormControl('',Validators.required),
+    requisitos : new FormControl('',Validators.required),
+    lugar : new FormControl('',Validators.required),
+    id_tipoCompetencias : new FormControl('',Validators.required),
+    email : new FormControl('',Validators.required),
+    costo : new FormControl('',Validators.required),
+  });
+
+  ngOnInit(): void {
+      let competenciaEditId = this.activaterouter.snapshot.paramMap.get('id');
+      if (competenciaEditId !== null) {
+        this.getData(parseInt(competenciaEditId, 10));
+      } else {
+        this.getData(0);
+      }
+      console.log(competenciaEditId);
+  }
+
+  getData(id:Number){
+    this.apiService.getCompetenciasById(id).subscribe(data=>{      
+      this.dataCompetencia = data;
+      this.editarForm.setValue({
+        'nombre': this.dataCompetencia.nombre || '',
+        'descripcion': this.dataCompetencia.descripcion || '',
+        'encargado': this.dataCompetencia.encargado || '',
+        'fechaFin': this.dataCompetencia.fechaFin || '',
+        'fechaIni': this.dataCompetencia.fechaIni || '',
+        'requisitos': this.dataCompetencia.requisitos || '',
+        'lugar': this.dataCompetencia.lugar || '',
+        'id_tipoCompetencias': this.dataCompetencia.id_tipoCompetencia.toString() || '',
+        'email': this.dataCompetencia.email || '',
+        'costo': this.dataCompetencia.email || '',
+      })
+      console.log(this.editarForm.value);
+    })
+  }
+
+  submit(datos: any){
+    console.log(datos);
+    
+  }
+
+  editarCompetencia(form:any, id:Number){
+    console.log(form);
+    console.log(id);
+    
+    Swal.fire({
+      title: '¿Estás seguro de editar la competencia?',
+      text: "No se podra deshacer la acción",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.editar(form,id);
+        Swal.fire(
+          'Editado!',
+          'Se ha editado la competencia con éxito',
+          'success'
+        ).then(() => {
+          this.router.navigate(['/admin/competencias']);
+        });
+      }
+    });
+  }
+  editar(data:CompetenciaEditI,id:Number){
+    this.apiService.putCompetencia(data,id).subscribe(data=>{
+      console.log(data);
+    })
+  }
 }
