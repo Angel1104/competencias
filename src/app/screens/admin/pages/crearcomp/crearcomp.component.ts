0771ac754fcf25bc6477bc2cdf8735001b0877bc
VinniMagne <vinni.magne@gmail.com>
2023-12-21 12:44:37 -0400

    comp hora ini fechas

--- src/app/screens/admin/pages/crearcomp/crearcomp.component.ts
+++ src/app/screens/admin/pages/crearcomp/crearcomp.component.ts
@@ -44,11 +44,49 @@ export class CrearcompComponent {
       email: ['', [Validators.required, Validators.pattern(/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/)]],
       umss: [false, Validators.required],
       horaInicio: ['',Validators.required],
-      horaFin: ['',Validators.required]
+      horaFin: ['', [Validators.required, this.validateHoraFin]],
     });
   }
 
   //Controles
+  validateHoraFin = (control: FormControl) => {
+    if (!this.crearForm) {
+      return { invalidHoraFin: false };
+    }
+  
+    const horaFin = control.value;
+    const ini = this.crearForm.get('horaInicio');
+  
+    if (!ini || !ini.value) {
+      return { invalidHoraFin: true, horaInicioVacia: true };
+    }
+  
+    const horaInicio = ini.value;
+  
+    return horaFin > horaInicio ? null : { invalidHoraFin: true };
+  };
+  getHoraFinErrorMessage() {
+    const fin = this.crearForm.get('horaFin');
+    const ini = this.crearForm.get('horaInicio');
+  
+    if (!fin || !ini) {
+      return 'Error en el formulario';
+    }
+  
+    if (fin.hasError('required') || ini.hasError('required')) {
+      return 'Llene primero la hora inicio';
+    }
+  
+    return fin.hasError('invalidHoraFin') ? 'La hora de fin debe ser mayor a la hora de inicio' : '';
+  }
+  getHoraIniErrorMessage() {
+    const ini = this.crearForm.get('horaInicio');
+    if (!ini) {return 'Error en el formulario';}
+    if (ini.hasError('required')) {
+    return 'Este campo es obligatorio';
+    }
+    return '';
+}
   validateFechaIni(control: FormControl) {
     const fechaInicio = new Date(control.value);
     const today = new Date();
